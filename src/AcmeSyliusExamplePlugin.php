b4b8372e1606d398e530e2e854f67f6f1aab01d0
Federico Urbinelli <f.urbinelli@netrising.com>
2025-04-01 18:07:51 +0200

    add igfs api code

--- src/AcmeSyliusExamplePlugin.php
+++ src/AcmeSyliusExamplePlugin.php
@@ -2,12 +2,12 @@
 
 declare(strict_types=1);
 
-namespace Acme\SyliusExamplePlugin;
+namespace Netrising\SyliusIGFSGatewayPlugin;
 
 use Sylius\Bundle\CoreBundle\Application\SyliusPluginTrait;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-final class AcmeSyliusExamplePlugin extends Bundle
+final class NetrisingSyliusIGFSGatewayPlugin extends Bundle
 {
     use SyliusPluginTrait;
 
