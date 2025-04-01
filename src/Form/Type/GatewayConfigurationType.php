4de0b7b3a172258a33785e5b188352e54931b1d9
Federico Urbinelli <f.urbinelli@netrising.com>
2025-04-01 18:41:13 +0200

    fixes, import code from https://github.com/vanssata/SyliusPagOnlineImpresePlugin

--- src/Form/Type/GatewayConfigurationType.php
+++ src/Form/Type/GatewayConfigurationType.php
@@ -13,7 +13,7 @@ use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Validator\Constraints\Range;
 use Netrising\SyliusIGFSGatewayPlugin\Payum\PagOnlineImpreseApi;
 
-final class PagOnlineImpreseGatewayConfigurationType extends AbstractType
+final class GatewayConfigurationType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
